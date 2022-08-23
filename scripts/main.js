284dfcfb6adefebbf7686b0135f37cf5c8fa0787
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-08-23 20:38:09 +0300

    add menu

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,12 @@ button.addEventListener('click', () => {
   button.classList.toggle('--clicked')
   burgerMenu.classList.toggle('--opened')
 })
+
+const burgerLinks = Array.from(document.querySelectorAll('.burger-menu__link'))
+burgerLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    button.classList.remove('--clicked')
+    burgerMenu.classList.remove('--opened')
+  })
+})
+
